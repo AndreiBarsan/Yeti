0663c08056cccea1ec9f07879a9c3a91748a638f
Andrei Bârsan <andrei@siegedog.com>
2014-06-10 23:59:19 +0300

    Removed old run.bad script which was no longer working anyway since migrating to Maven for dependency management

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem This is a quick script to run the already-compiled engine from the command line on windows.
-java -cp bin/;lib/jogamp-all-platforms/jar/jogl-all.jar;lib/jogamp-all-platforms/jar/gluegen-rt.jar barsan.opengl.editor.App
